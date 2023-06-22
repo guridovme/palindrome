a10e167029841c70985b66a1670b3efd751f4b42
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-23 00:12:57 +0200

    1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const palindromeInputNode=document.getElementById("palindromeInput");
+const addButtonNode=document.getElementById("addButton");
 
+function getPalindromeFromUser () {
+    return parseInt (palindromeInputNode);
+}
+
+
+//function palindrome
+
+// функция-обработчик, которая будет вызвана при нажатии на кнопку Добавить
+function addButtonHandler() {
+    // сохранияем в переменную currentAmount введенную сумму
+    const currentPalindrome = getPalindromeFromUser();
+    if (!currentPalindrome) {
+    return;
+    }
+
+ 
+
+}
+
+
+// Привязка функций-обработчиков к кнопкам
+addButtonNode.addEventListener("click",addButtonHandler);
