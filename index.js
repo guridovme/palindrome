e114dabb36f44b9535257d0c1ceedddcacc54b97
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-24 22:42:03 +0200

    1

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-const inputNode=document.getElementById('palindromeInput');
-const addButtonNode=document.getElementById("addButton");
-
-function getPalindromeFromUser () {
-    return parseInt (inputNode.value);
+function myFunction(){
+    var a = parseInt(document.getElementById('palindromeInput').value);
+    document.getElementById('output').innerHTML = a;
 }
-
-function addButtonHandler() {
-    
-    const currentAmount = getPalindromeFromUser();
-    if (!currentAmount) {
-    return;
-    }
-
-    console.log(currentAmount);
-
-    
-    
-}
-
-addButtonNode.addEventListener("click",addButtonHandler);
-
-
-
-// document.getElementById('addButton').onclick = getInput();
-
-// function getInput (){
-//     let a = document.getElementById('palindromeInput').value;
-//     console.log(a);
-// };
