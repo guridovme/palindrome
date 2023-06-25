afcf84e20596c0b52e449fb99e7e1c0c615909f7
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-25 12:14:32 +0200

    2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const buttonNode = document.getElementById('addButton');
 const inputNode = document.getElementById('palindromeInput');
+const outputNode = document.getElementById('palindromeOutput');
 
 buttonNode.addEventListener('click', function (){
 const palindrome = inputNode.value;
 console.log(palindrome);
+outputNode.innerText = palindrome;
 });
