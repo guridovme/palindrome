5c22467dd214057f7b4f849160ae44711fe924cd
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-25 21:22:19 +0200

    3

--- index.js
+++ index.js
@@ -10,11 +10,3 @@ outputNode.innerText = palindrome;
 
 
 
-function multiple (){
-    const a = 2;
-    const b = 2;
-    multipleResult = a * b;
-    console.log('Multiple Result', a , '*', b ,'=', multipleResult);
-
-}
-multiple ();
---
+++ multiple.js
@@ -0,0 +1,10 @@
+
+
+function multiple (){
+    const a = 2;
+    const b = 2;
+    multipleResult = a * b;
+    console.log('Multiple Result', a , '*', b ,'=', multipleResult);
+
+}
+multiple ();
