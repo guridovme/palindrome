c7070eb7d6dc61c46a6563c1daf0fc0a8435625a
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-25 12:39:26 +0200

    2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ outputNode.innerText = palindrome;
 function multiple (){
     const a = 2;
     const b = 2;
-    multipleResult = a*b;
+    multipleResult = a * b;
     console.log('Multiple Result', a , '*', b ,'=', multipleResult);
 
 }
