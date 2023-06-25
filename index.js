076c7bb73db0c3b4beb1c8f34abb437897fbcc51
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-25 12:06:26 +0200

    2

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const addButtonNode = document.getElementById("addButton");
-const inputNode = document.getElementById("palindromeInput");
+const buttonNode = document.getElementById('addButton');
+const inputNode = document.getElementById('palindromeInput');
 
-
-console.log(addButtonNode);
-console.log(inputNode);
-
-addButtonNode.addEventListener("click",addButtonHandler);
-
-function addButtonHandler() {
-    
-    const currentAmount = inputNode.value;
-   
-}
-
-addButtonHandler();
-
-console.log('currentAmount=',currentAmount);
+buttonNode.addEventListener('click', function (){
+const palindrome = inputNode.value;
+console.log(palindrome);
+});
