535ee0549ef464e2eb987987ee1e7674a97031e1
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-29 01:56:46 +0200

    1

--- builddate.js
+++ builddate.js
@@ -1,8 +1,8 @@
 function buildDate (day, month, year){
     
-    return `${day} / ${month} / ${year}`;
+    return `${day}/${month}/${year}`;
     
 }
 
 
-console.log(buildDate('12','06','2023'));
+console.log(buildDate(12,06,2023));
