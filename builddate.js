06f7f5dff42a59a588aa1d307bb585067ad9360a
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-28 17:19:21 +0200

    2

---
+++ builddate.js
@@ -0,0 +1,8 @@
+function buildDate (day, month, year){
+    
+    return 
+    
+}
+
+let x = buildDate('22', '06', '2002');
+console.log(x);
--- multiple.js
+++ multiple.js
@@ -1,13 +1,15 @@
-function multiple (){
+function multiply (){
     const a = 5;
     const b = 5;
-    multipleResult = a * b;
-    console.log('Multiple Result', a , '*', b ,'=', multipleResult);
+    c = a * b;
+    console.log('Result:', a , '*', b ,'=', c);
 
 }
-multiple ();
+multiply ();
 
 
-function multiply(a, b) { return a * b; }
-let x = multiply(5, 6);
-console.log(x);
+function multiple(a, b) { 
+    return a * b; 
+}
+let c = multiple(5, 6);
+console.log(c);
