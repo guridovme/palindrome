08a4601485b5d4848a40a52ca61012caa1ea7726
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-06-29 03:36:31 +0200

    3

--- builddate.js
+++ builddate.js
@@ -1,8 +1,13 @@
-function buildDate (day, month, year){
-    
-    return `${day}/${month}/${year}`;
-    
+let day = 3;
+let month = 12;
+let year = 2023;
+const totalLength = 2;
+
+function dayCheck(day,totalLength) {
+  return String(day).padStart(totalLength, '0');
 }
-
-
-console.log(buildDate(12,6,2023));
+function monthCheck(month,totalLength) {
+  return String(month).padStart(totalLength, '0');
+}
+ 
+console.log(`${dayCheck(day,totalLength)}/${monthCheck(month,totalLength)}/${year}`);
--- multiple.js
+++ multiple.js
@@ -1,15 +1,4 @@
-// function multiply (){
-//     const a = 5;
-//     const b = 5;
-//     c = a * b;
-//     console.log('Result:', a , '*', b ,'=', c);
-
-// }
-// multiply ();
-
-
 function multiple(a, b) { 
     return a * b; 
 }
-let c = multiple(5, 6);
-console.log(c);
+console.log(multiple(5,12));
