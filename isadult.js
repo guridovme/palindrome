8f87d0a89ef70ee20615a1a5158ff2e38db8b639
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-07-05 19:08:37 +0200

    Create isadult.js

---
+++ isadult.js
@@ -0,0 +1,14 @@
+let age = 1;
+
+function isAdult (age) {
+  if (age >= 18) {
+  return true;
+  } else {
+  return false;
+  }
+}
+console.log(isAdult(age));
+
+//let age = 1;
+//function isAdult (age) {return age >= 18;}
+//console.log(isAdult(age));
